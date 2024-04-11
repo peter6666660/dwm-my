b7d5634fb13b4a2b64bc7a9bd37ede54c59742c9
peter <peter6666660@gmail.com>
2024-04-11 14:22:24 +0800

    音量

--- config.h
+++ config.h
@@ -59,11 +59,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *mut[]  = { "amixer", "set" ,"Master", "mute", NULL };
+static const char *audiolower[]  = { "amixer", "set", "Master", "5%-", "unmute", NULL };
+static const char *audioraise[]  = { "amixer", "set" ,"Master" ,"5%+" ,"unmute", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ 0,      	  XF86XK_AudioMute,   	   spawn,          {.v=mut}},
+	{ 0,      	XF86XK_AudioLowerVolume,   spawn,          {.v=audiolower}},
+	{ 0,      	XF86XK_AudioRaiseVolume,   spawn,          {.v=audioraise}},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
