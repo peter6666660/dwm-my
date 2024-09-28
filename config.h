fce4b546647b873044f3fb11ad77c627e4efb8f3
peter <peter6666660@gmail.com>
2024-09-29 00:02:17 +0800

    增加dwm-statuscmd 补丁 并且 使用dwmblocks 管理状态栏

--- config.h
+++ config.h
@@ -57,22 +57,23 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#define STATUSBAR "dwmblocks"
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *mut[]  = { "pactl", "set-sink-mute" ,"@DEFAULT_SINK@", "toggle", NULL };
-static const char *audiolower[]  = { "amixer", "set", "Master", "5%-", "unmute", NULL };
-static const char *audioraise[]  = { "amixer", "set" ,"Master" ,"5%+" ,"unmute", NULL };
-static const char *statuscmd[] = { "polybar", "example", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ 0,      	  XF86XK_AudioMute,   	   spawn,          {.v=mut}},
-	{ 0,      	XF86XK_AudioLowerVolume,   spawn,          {.v=audiolower}},
-	{ 0,      	XF86XK_AudioRaiseVolume,   spawn,          {.v=audioraise}},
+  // 静音切换
+	{ 0,      	  XF86XK_AudioMute,   	   spawn,          SHCMD("bash ~/.dwm/scripts/volume_control.sh toggle")},
+  // 减少音量
+	{ 0,      	XF86XK_AudioLowerVolume,   spawn,          SHCMD("bash ~/.dwm/scripts/volume_control.sh down")},
+  // 增加音量
+	{ 0,      	XF86XK_AudioRaiseVolume,   spawn,          SHCMD("bash ~/.dwm/scripts/volume_control.sh up")},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -113,7 +114,9 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
